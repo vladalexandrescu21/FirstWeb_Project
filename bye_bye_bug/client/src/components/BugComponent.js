cd0bb3791193f8f16c592bcf23ad81109bbcef89
vladalexandrescu21 <vlad.alexandrescu21@gmail.com>
2023-01-15 23:05:00 +0200

    CSS

--- bye_bye_bug/client/src/components/BugComponent.js
+++ bye_bye_bug/client/src/components/BugComponent.js
@@ -6,13 +6,17 @@ import "./login.css";
 
 const BugComponent = (props) => {
     console.log("bugs", props.bugs, typeof props.bugs);
+
+    function SelectBug(){
+
+    }
   return (
-    <div>
+    <div className="bugDivM">
       {props.bugs.map((x) => (
         <div key={x.id} className="bugDiv">
           Bug name: {x.bugName}, Project name: {x.projectName}, Who to solve:{" "}
-          {x.whoToSolve}, Is solved: {x.isSolved},Severity: {x.severity}
-          ,Priority: {x.priority}
+          {x.whoToSolve}, Is solved: {x.isSolved}, Severity: {x.severity}
+          , Priority: {x.priority}, 
           Description: {x.description}, Commit link: {x.commitLink}
         </div>
       ))}
